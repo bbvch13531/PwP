4694b092adf06c90381136ce8765ce8e19106ed1
suitelab <yoonwoosik12@naver.com>
2018-11-14 16:06:16 +0900

    Added myjs.js

---
+++ myjs.js
@@ -0,0 +1,11 @@
+ImageSwitch = new Array();
+for (i = 0; i<6;i++){
+	ImageSwitch[i] = 'image_'+i+'.png';
+}
+randnum = Math.round(Math.random()*5)
+randimg = "background-image: url('images/image_"+randnum+".png');";
+
+function swapImage(){
+	var menu = document.getElementById("backgroundImg");
+	menu.setAttribute("style", randimg)
+}
